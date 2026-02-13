27c2662a2c36603de89c0bf1e531ebd20b50142f
kimsrik <rltmrl2@gmail.com>
2026-02-13 10:07:39 +0900

    back // page JUnit test code added

--- back/src/test/java/com/example/demo/repository/TodoRepositoryTests.java
+++ back/src/test/java/com/example/demo/repository/TodoRepositoryTests.java
@@ -6,6 +6,10 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.example.demo.domain.Todo;
 
@@ -51,7 +55,7 @@ public class TodoRepositoryTests {
 		log.info("--------------- : " + todo);
 	}
 	*/
-	
+	/*
 	@Test
 	public void testModify() {
 		Long tno = 33L;
@@ -71,6 +75,19 @@ public class TodoRepositoryTests {
 		Long tno = 32L;
 		
 		todoRepository.deleteById(tno);
+	}
+	*/
+	
+	// 페이징 테스트
+	
+	public void testPaging() {
+		Pageable pageable = PageRequest.of(2, 15, Sort.by("tno").descending());
+										// (몇 페이지, 페이지 당 글 몇개?, 정렬)
+		Page<Todo> result = todoRepository.findAll(pageable);
+		
+		log.info("총 객체수 : " + result.getTotalElements());
+		
+		result.getContent().stream().forEach( todo -> log.info(todo) );
 	}
 	
 }
