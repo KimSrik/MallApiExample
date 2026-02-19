85022b5779325ad32009822a2780f7ec7b5389f0
kimsrik <rltmrl2@gmail.com>
2026-02-19 10:43:45 +0900

    front // strictmode delete

--- front/src/components/todo/ReadComponent.jsx
+++ front/src/components/todo/ReadComponent.jsx
@@ -18,6 +18,9 @@ const ReadComponent = ( {tno} ) => {
       setTodo(data);
     })
   }, [tno]);
+  // 비동기 통신. 
+  // 데이터 변화가 있으면 데이터가 다 오면 한번만 실행함.
+  // 데이터 변화에 취약하기 때문에 비동기 통신을 씀.
 
   return (
     <div>
