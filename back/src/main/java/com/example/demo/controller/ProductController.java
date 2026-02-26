f8c6a8146975111b4afbc753ed47a920c980cf1e
kimsrik <rltmrl2@gmail.com>
2026-02-26 13:13:07 +0900

    back // viewFileGET added

--- back/src/main/java/com/example/demo/controller/ProductController.java
+++ back/src/main/java/com/example/demo/controller/ProductController.java
@@ -3,6 +3,10 @@ package com.example.demo.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,4 +41,11 @@ public class ProductController {
 		
 		return Map.of("RESULT", "SUCCESS");
 	}
+	
+	@GetMapping("/view/{fileName}")
+	public ResponseEntity<Resource> viewFileGET(@PathVariable(name = "fileName") String fileName){
+		
+		return fileUtil.getFile(fileName);
+	}
+	
 }
